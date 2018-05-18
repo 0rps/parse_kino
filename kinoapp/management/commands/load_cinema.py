75fcb750354b5ce97296b4c83adb315288ea8eb7
MogilnikovAB <tuxrps@gmail.com>
2018-05-18 19:15:38 +0300

    [add] models

---
+++ kinoapp/management/commands/load_cinema.py
@@ -0,0 +1,52 @@
+from django.core.management.base import BaseCommand
+from django.db.models import ObjectDoesNotExist
+from django.db import transaction
+
+from kinoapp import models
+from kinoapp import parser
+
+
+class Command(BaseCommand):
+    help = 'load cinema'
+
+    def handle(self, *args, **options):
+        for min_cinema in self.load_cinema_list():
+            afisha_id = min_cinema.cid
+
+            try:
+                models.Cinema.objects.get(pk=afisha_id)
+            except ObjectDoesNotExist:
+                pass
+            else:
+                continue
+
+            max_cinema = self.load_additional_cinema_info(afisha_id)
+            if max_cinema is None:
+                continue
+
+            cinema = models.Cinema()
+            cinema.afisha_id = afisha_id
+            cinema.name = min_cinema.name
+            cinema.address = max_cinema.address
+            if min_cinema.metro:
+                cinema.metro = min_cinema.metro
+            if max_cinema.rating:
+                try:
+                    float(max_cinema.rating)
+                except ValueError:
+                    pass
+                else:
+                    cinema.rating = max_cinema.rating
+            if max_cinema.votes:
+                cinema.votes = max_cinema.votes
+            with transaction.atomic():
+                cinema.save()
+            self.stdout.write("Добавлен кинотеатр: " + cinema.name)
+
+    def load_additional_cinema_info(self, cinema_id):
+        add_cinema = parser.CinemaPageParser().parse(cinema_id)
+        return add_cinema
+
+    def load_cinema_list(self):
+        cinema_list = parser.CinemaListParser().parse()
+        return cinema_list
--- kinoapp/models.py
+++ kinoapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Cinema(models.Model):
+    name = models.CharField(max_length=128)
+    address = models.CharField(max_length=256)
+    metro = models.CharField(max_length=64)
+    votes = models.IntegerField(null=True)
+    rating = models.FloatField(null=True)
+
+    afisha_id = models.IntegerField(null=True, db_index=True)
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=128)
+    info = models.CharField(max_length=512)
+
+    imdb_votes = models.IntegerField(null=True)
+    imdb_rating = models.FloatField(null=True)
+
+    afisha_id = models.IntegerField(null=True, db_index=True)
+
+
+class Showtime(models.Model):
+    price = models.IntegerField()
+    time = models.DateTimeField()
+    cinema = models.ForeignKey(Cinema, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
